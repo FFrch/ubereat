b7e217f3d43677fafe94cef7c01d2a69e9234047
Fanny Farrouch <100222958+FFrch@users.noreply.github.com>
2023-05-02 15:04:10 +0200

    Restaurant searchbar feature ongoing

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -2,7 +2,7 @@
   <div class="home--page">
     <div class="header">
       <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="">
-      <input type="search" placeholder="De quoi avez-vous envie?">
+      <input v-model="user_search_restaurant" type="search" placeholder="De quoi avez-vous envie?">
     </div>
     <div class="banner"></div>
     <restaurant-row v-for="(data, i) in data_restautant" :key="i" :three_restaurant="data"/>
@@ -11,53 +11,67 @@
 </template>
 
 <script>
-// Imports
-import BDD from '../BDD.js'
-import { onMounted, ref } from "vue";
+  // Imports
+  import BDD from '../BDD.js'
+  import { onMounted, ref, watch } from "vue";
 
-// Components
-import RestaurantRow from "../components/RestaurantRow.vue";
+  // Components
+  import RestaurantRow from "../components/RestaurantRow.vue";
 
-export default {
- name: 'HomePage',
- components : {
-  RestaurantRow,
- },
- setup() {
-  class Restaurant {
-    constructor(name, note, image, drive_time) {
-      this.name = name
-      this.note = note
-      this.image = image
-      this.drive_time = drive_time
-    }
-  }
+  export default {
+    name: 'HomePage',
+    components : {
+      RestaurantRow,
+    },
+    setup() {
+      class Restaurant {
+        constructor(name, note, image, drive_time) {
+          this.name = name
+          this.note = note
+          this.image = image
+          this.drive_time = drive_time
+        }
+      }
 
-  let data_restautant = ref([]);
+      let data_restautant = ref([]);
+      let all_restaurant = [];
 
-  const makeDataRestaurant = () => {
-    let three_restaurant = [];
+      const makeDataRestaurant = () => {
+        let three_restaurant = [];
 
-    for (const restaurant of BDD){
-      const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time);
+        for (const restaurant of BDD){
+          const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time);
 
-      if(three_restaurant.length === 2){
-        three_restaurant.push(new_restaurant);
-        data_restautant.value.push(three_restaurant);
-        three_restaurant = [];
-      } else {
-        three_restaurant.push(new_restaurant);
+          // Push all new restaurant in all_restaurant array
+          all_restaurant.push(new_restaurant)
+
+          if(three_restaurant.length === 2){
+            three_restaurant.push(new_restaurant);
+            data_restautant.value.push(three_restaurant);
+            three_restaurant = [];
+          } else {
+            three_restaurant.push(new_restaurant);
+          }
+        }
+      }
+
+      // Searchbar
+      let user_search_restaurant = ref('');
+
+      watch(user_search_restaurant, new_value => {
+        let regex = RegExp(new_value);
+        let search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name))
+        console.log(search_restaurant)
+      })
+
+      onMounted(makeDataRestaurant);
+
+      return {
+        data_restautant,
+        user_search_restaurant,
       }
     }
   }
-
-  onMounted(makeDataRestaurant);
-
-  return {
-    data_restautant,
-  }
- }
-}
 </script>
 
 <style lang="scss">
@@ -89,7 +103,6 @@ export default {
       background-image: url("https://ubernewsroomapi.10upcdn.com/wp-content/uploads/sites/345/2016/03/blog_960x540.jpg");
       background-size: cover;
       background-position: top center;
-
     }
   }
 </style>
